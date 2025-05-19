2e60c190555d8beef64052b7ca26348dd6d6e9f7
satrianugrohoputra <satrianugrohoputra.xa6@gmail.com>
2025-05-19 22:08:23 +0700

    betulin apikey

--- assets/js/login-google.js
+++ assets/js/login-google.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGBPm_QY_r3BBV9xqpt6SQvQdK9nhKGXU",
+  apiKey: "AIzaSyDGBPm_QY_r3B8V9xqpt6SQvQdK9nhKGXU",
   authDomain: "wellnesshub-841fd.firebaseapp.com",
   projectId: "wellnesshub-841fd",
   storageBucket: "wellnesshub-841fd.appspot.com",
--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 
 // Konfigurasi Firebase kamu
 const firebaseConfig = {
-  apiKey: "AIzaSyDGBPm_QY_r3BBV9xqpt6SQvQdK9nhKGXU",
+  apiKey: "AIzaSyDGBPm_QY_r3B8V9xqpt6SQvQdK9nhKGXU",
   authDomain: "wellnesshub-841fd.firebaseapp.com",
   projectId: "wellnesshub-841fd",
   storageBucket: "wellnesshub-841fd.appspot.com",
