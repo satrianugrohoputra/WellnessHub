6337ebfa1d3cc3cf4802ea8fa68c9cdc1b7221e1
satrianugrohoputra <satrianugrohoputra.xa6@gmail.com>
2025-05-26 14:30:45 +0700

    Inisialisasi project + push awal

---
+++ env.js
@@ -0,0 +1,10 @@
+// assets/js/env.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGBPm_QY_r3BBV9xqpt6SQvQdK9nhKGXU",
+  authDomain: "wellnesshub-841fd.firebaseapp.com",
+  projectId: "wellnesshub-841fd",
+  storageBucket: "wellnesshub-841fd.appspot.com",
+  messagingSenderId: "766630298756",
+  appId: "1:766630298756:web:5202acf967f9ba9832eed1",
+  measurementId: "G-BKMVRNQQF0"
+};
